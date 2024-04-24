6e1f7826e6e2cf02b1c0392a64fa3806d6cc2fde
Suyash Mahajan <suyash608.co@gmail.com>
2024-04-24 23:47:12 +0530

    College Registration Portal

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; <?php echo date('Y');?> | By : <a href="" target="_blank">Indian Institute Of Information Technology, Nagpur</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ admin/includes/header.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(0);
+?>
+
+<!-- HEADER END-->
+<div class="navbar navbar-inverse set-radius-zero">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#" style="color:#fff; font-size:24px;4px; line-height:24px; ">
+               <center>  <img src="https://iiitn.ac.in/images/NewsEvents/84/NewsDocument.png" alt="Your Image" style="width: 40px; height: 47px; margin-right: 60px;"></center>
+                   IIIT Nagpur Course Registration Portal
+            </a>
+
+        </div>
+    </div>
+</div>
